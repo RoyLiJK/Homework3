a98e813df5e229b7e463e7448e55a9b05754e575
Roy Li <xiaolongli1988@gmail.com>
2016-03-20 21:58:06 -0400

    add a bit stuff

--- Homework3/Main.cpp
+++ Homework3/Main.cpp
@@ -1,13 +1,27 @@
 #include<iostream>
 #include<string>
 #include"Address.h"
+#include"Contact.h"
 using namespace std;
 
 int main()
 {
-	Address joe;
-	joe.input();
-	joe.output();
+	string Arr[10];
+	
+	
+	cout << "Enter up to 10 Contacts (enter 0 to exit):  " << endl;
+	for (int i = 0; i < 10;i++)
+	{
+		
+			cin >> Arr[i];
+		
+			if (Arr[i] == "0")
+				break;
+	}
 
+	for (int j = 0; j < 10; j++)
+	{
+		cout << "Your contact are: " << Arr[j] << endl;
+	}
 	return 0;
 }
