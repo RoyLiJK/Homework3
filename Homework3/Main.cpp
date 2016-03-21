7107e6ecd881534ff182e362158ab15963e98a5f
Roy Li <xiaolongli1988@gmail.com>
2016-03-21 10:36:19 -0400

    fix some stuff

--- Homework3/Main.cpp
+++ Homework3/Main.cpp
@@ -6,22 +6,26 @@ using namespace std;
 
 int main()
 {
-	string Arr[10];
-	
+	Contact Arr[10];
+	string s;
 	
 	cout << "Enter up to 10 Contacts (enter 0 to exit):  " << endl;
 	for (int i = 0; i < 10;i++)
 	{
+		cin >> s;
+		Arr[i].setfirstname(s);
 		
-			cin >> Arr[i];
-		
-			if (Arr[i] == "0")
-				break;
+
+			/*if (Arr[i].setfirstname("0") || Arr[i].setlastname("0"))
+				break;*/
 	}
 
-	for (int j = 0; j < 10; j++)
-	{
-		cout << "Your contact are: " << Arr[j] << endl;
-	}
+	
+		Arr[2].getfirstname();
+		
+
+
+
+	
 	return 0;
 }
